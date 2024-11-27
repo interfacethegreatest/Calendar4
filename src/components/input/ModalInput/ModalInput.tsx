2343c1b16924f35f2c7648e1597415d1b91788c7
Interfacethegreatest <ellingtondrew@yahoo.co.uk>
2024-11-27 22:52:43 +0000

    27/11/2024-2

--- src/components/input/ModalInput/ModalInput.tsx
+++ src/components/input/ModalInput/ModalInput.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { motion } from 'framer-motion';
 import { Poppins } from 'next/font/google';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { styleText } from 'util';
 import style from "./ModalInput.module.css"
+import outsideClick from './outsideClick';
 
 const font = Poppins({
   subsets: ["latin"],
@@ -29,20 +30,25 @@ const ModalInput: React.FunctionComponent<IModalInputProps> = (props) => {
   const handleClickedContent = () => {
     setClicked(true); // Toggle state
   };
-
+  function unclickContent(){
+    setClicked(false);
+  };
+  const ref = useRef();
+  outsideClick(ref, () => unclickContent());
   return (
     <div
       onClick={handleClickedContent}
-      id={style.inputContainer}
+      id={clicked ? style.glowingInputContainer : style.inputContainer}
+      ref={ref}
     >
       <motion.span
         initial={{ x: 0, y: "50%", fontSize: "1.1rem", color: "rgb(22, 60, 47)" }}
         animate={
           clicked
-            ? { x: 0, y: "-20%", fontSize: "0.8rem", color: "rgb(57, 148, 116)"}
+            ? { x: 0, y: "-20%", fontSize: "0.65rem", color: "rgb(57, 148, 116)"}
             : { x: 0, y: "0%", fontSize: "1.1rem", color: "rgb(106, 106, 106)" }
         }
-        transition={{ duration: 0.2 }}
+        transition={{ duration: 0.1 }}
         style={{
           position: "absolute",
           left: "10px",

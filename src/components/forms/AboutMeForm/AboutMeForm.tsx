40da59880c7ded76e2a68873c14c68818ed9dcb3
interfacethegreatest <ellingtondrew@yahoo.co.uk>
2025-01-28 19:20:24 +0000

    28/01/2025

--- src/components/forms/AboutMeForm/AboutMeForm.tsx
+++ src/components/forms/AboutMeForm/AboutMeForm.tsx
@@ -3,6 +3,19 @@ import * as React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import style from './style.module.css';
+import { motion } from 'framer-motion';
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { useState } from 'react';
+import ModalInput from '@/components/input/ModalInput/ModalInput';
+import { CiLock } from 'react-icons/ci';
+import SlideButtonSubmit from '@/components/buttons/auth/slideButtonSubmit';
+import { AiFillLock } from 'react-icons/ai';
+import { Poppins } from 'next/font/google';
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 interface IAboutMeFormProps {
 }
@@ -11,7 +24,7 @@ const FormSchema = z.object({
   AboutMe: z
     .string()
     .min(1, { message: "Description is required." })
-    .max(1050, { message: "Description must not exceed 1050 characters." })
+    .max(525, { message: "Too many characters." })
     .optional()
     .or(z.literal('')),
 });
@@ -19,6 +32,16 @@ const FormSchema = z.object({
 type FormSchemaType = z.infer<typeof FormSchema>;
 
 const AboutMeForm: React.FunctionComponent<IAboutMeFormProps> = (props) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [animation, setAnimation] = useState(true);
+  const nextSlide = () => {
+    console.log(currentSlide + 1)
+    setCurrentSlide((prev) => prev + 1);
+  };
+  const prevSlide = () => {
+    console.log(currentSlide -1)
+    setCurrentSlide((prev) => prev - 1);
+  };
   const {
     register,
     handleSubmit,
@@ -29,7 +52,7 @@ const AboutMeForm: React.FunctionComponent<IAboutMeFormProps> = (props) => {
   });
   const onSubmit: SubmitHandler<FormSchemaType> = async (values ) => {
     try {
-        
+        console.log(values)
     } catch (error) {
         alert("Error during submit: "+ error);
     }
@@ -37,15 +60,60 @@ const AboutMeForm: React.FunctionComponent<IAboutMeFormProps> = (props) => {
   return <>
   <form id={style.innerContainer} action="" onSubmit={handleSubmit(onSubmit)}>
      <div id={style.leftArrowContainer}>
-
+      { currentSlide != 0 && (
+        <motion.div title="Slide Back" onClick={prevSlide} id={style.iconHolder}>
+         <div id={style.iconStyle}><MdOutlineKeyboardArrowLeft/></div>
+        </motion.div>
+      )}
      </div>
      <div id={style.mainBodyContainer}>
-     <h2 id={style.title}><b>Describe yourself briefly:</b></h2>
-     <h5 id={style.subtitle}>Provide text for your about you section:</h5>
+      {
+        currentSlide === 0 && (
+          <>
+          <h2 id={style.title} ><b>Describe yourself briefly:</b></h2>
+          <h5 id={style.subtitle}>Provide text for your about you section:</h5>
+          <br />
+          <ModalInput
+          name="AboutMe"
+          label="About Me:"
+          type="text"
+          icon={<CiLock />}
+          register={register}
+          error={errors?.AboutMe?.message}
+          disabled={isSubmitting}
+          height={250}
+          topLocation={20}
+          inputLength={525} placeholder={''}/>
+          </> 
+        )
+      }
+      {
+        currentSlide === 4 && (
+          <>
+           <SlideButtonSubmit
+            type="submit"
+            slide_text="Save your details"
+            text={"Save"}
+            icon={<AiFillLock />}
+            width="250px"
+            disabled={isSubmitting}
+            animation={animation} setScene={()=> null}/>
+          </> 
+        )
+      }
+      <div id={style.slideCountContainer}>
+      {Array.from({ length: currentSlide }, (_, index) => (
+       <div key={index} id={style.incrementDiv}></div>
+      ))}
+      </div>
      </div>
      <div id={style.rightArrowContainer}>
-
-     </div>
+     { currentSlide <= 3 && (
+        <motion.div title="Skip" onClick={nextSlide} id={style.iconHolder}>
+         <div id={style.iconStyle}><MdOutlineKeyboardArrowRight/></div>
+        </motion.div>
+      )}
+      </div>
   </form>
   </>;
 };

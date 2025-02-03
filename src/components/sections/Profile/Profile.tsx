f105115e09242f80db0b064782c1a1495aeb4826
interfacethegreatest <ellingtondrew@yahoo.co.uk>
2025-02-03 19:28:25 +0000

    03/02/2025

---
+++ src/components/sections/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import style from './style.module.css';
+
+
+interface IProfileProps {
+}
+
+const Profile: React.FunctionComponent<IProfileProps> = (props) => {
+  return <>
+  <div id={style.profile}>
+
+  </div>
+  </>;
+};
+
+export default Profile;
--- src/pages/user/[userId].tsx
+++ src/pages/user/[userId].tsx
@@ -15,69 +15,29 @@ import { isValidObjectId } from 'mongoose';
 import WebsiteButton from '@/components/buttons/websiteButton/websiteButton';
 import ProfileBody from '@/components/sections/ProfileBody/ProfileBody';
 import TiltModalAboutMe from '@/components/modals/TiltModalAboutMe/TiltModalAboutMe';
-
-
-const COLOURS = [
-  'rgba(159, 158, 158, 0.7)',
-  'rgba(159, 158, 158, 0.5)',  
-  'rgba(130, 129, 129, 0.35)', 
-  'rgba(189, 188, 188, 0.2)', 
-  'rgba(159, 158, 158, 0.17)'
-];
+import Profile from '@/components/sections/Profile/Profile';
 
 
 export default function user({userId, user}:{userId:string, user: InferGetServerSidePropsType<typeof getServerSideProps>}) {
-  const [windowSize, setWindowSize] = useState({
-    width: 0,
-    height: 0,
-  });
-  const colour = useMotionValue(COLOURS[0])
+  // Used to check logged in user or not,
   const { data : session } = useSession();
+  // Used to set the selection in the below the user profile,
   const [ selection, setSelection ] = useState([true, false, false, false]);
+  // Used to show the content in the modal to allow for editing the user profile.
   const [showContent, setShowContent] = useState(false);
+  // Used to show the content in the modal to edit the aboutYou section 
   const [showContentAboutMe, setShowContentAboutMe] = useState(false)
+  // Used in the modal to set and use a user defined image 
   const [ imageString, setImageString ] = useState(null);
+  // Used in the modal to set and use a user defined username, 
   const [ userString, setUserString ] = useState(null);
+  // Used in the modal to set and use a user defined description 
   const [ descriptionString, setDescriptionString ] = useState(null);
+  // Used in the modal to set and use a user defined website
   const [ websiteString, setWebsiteString ] = useState(null);
+  // Used in a useEffect to find if a profile is already liked by the user,
   const [isLiked, setIsLiked] = useState(false); // State to toggle the icon
-  const ref = useRef();
-  outsideClick(ref, ()=>setShowContent(false))
-  useEffect(() => {
-    animate(colour, COLOURS, {
-        ease: "easeInOut",
-        duration: 10,
-        repeat: Infinity,
-        repeatType: "mirror",
-    });
-  }, []);
-  useEffect(()=>{
-    /*console.log(session)
-    setImageString(session?.image);
-    setDescriptionString(session?.description)
-    */
-
-  },[session])
-   
-  useEffect(() => {
-    //Record the changes to window size upon window resize,
-    const handleResize = () => {
-      const width = document.documentElement.clientWidth;
-      const height = document.documentElement.clientHeight;
-
-      setWindowSize({ width, height });
-    };
-
-    // Attach the resize event listener
-    window.addEventListener("resize", handleResize);
-
-    // Call the function immediately to log the initial size
-    handleResize();
-
-    // Cleanup the event listener on component unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
+  
   function handleClick(arg0: number) {
     //after the user clicks next in the edit modal, move to the next page, setSelection(true) enables this.
     var newSelection = [false, false, false,false]
@@ -95,47 +55,16 @@ export default function user({userId, user}:{userId:string, user: InferGetServer
     }
   })
 
-  //set isLiked to true if the user is signed in, if the signed in user is not the profile being viewed, and if the person viewing is already following,
-  useEffect(() => {
-    if (session && session.id !== user._id) {
-      // Check if the session ID exists in the followers array
-      const isFollower = user.followers.includes(session.id);
-      setIsLiked(isFollower);
-    }
-  }, [session, user]); // Only re-run this effect when `session` or `user` changes
-  
-  console.log('isLiked : ' + isLiked);
+  // set isLiked to true if the user is signed in, if the signed in user is not the profile being viewed,
+  // and if the person viewing is already following the user being viewed ,
+ 
   return (<>
      <div id={style.main}>
       <Scene/>
          <div id={style.body}>
-          <motion.div id={style.profile}>
-            <div id={style.profileHeader}>
-              <div id={style.loader}>
-              </div>
-              <div id={style.profileImage}>
-                {
-                  /* if the user is signed in use the image from the userSession object, else use the DB user object,*/
-                  imageString ? <img id={style.image} src={ imageString } alt="" /> :
-                  <img id={style.image} src={ user.image } alt="" /> 
-                }
-              </div>
-              <br />
-            </div>
-            <motion.div id={style.profileBody}>
-              <ProfileBody setShowContent={setShowContent} 
-              session={session} userString={userString} 
-              descriptionString={descriptionString} 
-              name={user.name} 
-              Biography={user.Biography} 
-              following={user.following.length} 
-              followers={user.followers.length} 
-              userId={userId}
-              isLiked={isLiked}
-              setIsLiked={setIsLiked}/>
-            <WebsiteButton websiteString={websiteString!} session={session} user={user}/>
-            </motion.div>
-          </motion.div>
+          <div >
+            <Profile/>
+          </div>
           <div id={style.selector}>
             {
               selection[0] ? <ul id={style.selectedText}>About</ul> : <ul onClick={()=>handleClick(0)} id={style.selectorText}>About</ul>

5419364f2272bdfabfbca73c809ac5690966d215
interfacethegreatest <ellingtondrew@yahoo.co.uk>
2026-04-27 08:59:52 +0100

    27/04/2026

--- src/components/sections/BodyMain/BodyMain.tsx
+++ src/components/sections/BodyMain/BodyMain.tsx
@@ -5,13 +5,42 @@ type BodyMainProps = {
   selectedDate: Date | null;
 };
 
+const times = [
+  "12 AM",
+  "1 AM",
+  "2 AM",
+  "3 AM",
+  "4 AM",
+  "5 AM",
+  "6 AM",
+  "7 AM",
+  "8 AM",
+  "9 AM",
+  "10 AM",
+  "11 AM",
+  "12 PM",
+  "1 PM",
+  "2 PM",
+  "3 PM",
+  "4 PM",
+  "5 PM",
+  "6 PM",
+  "7 PM",
+  "8 PM",
+  "9 PM",
+  "10 PM",
+  "11 PM",
+];
+
 const BodyMain: React.FC<BodyMainProps> = ({ selectedDate }) => {
   const safeDate = selectedDate ?? new Date();
 
   const dayNumber = safeDate.getDate().toString();
-  const dayName = safeDate.toLocaleDateString("en-GB", {
-    weekday: "short",
-  }).toUpperCase();
+  const dayName = safeDate
+    .toLocaleDateString("en-GB", {
+      weekday: "short",
+    })
+    .toUpperCase();
 
   return (
     <div className={styles.main}>
@@ -25,6 +54,20 @@ const BodyMain: React.FC<BodyMainProps> = ({ selectedDate }) => {
         <div className={styles.bodyMainHeaderRight}>
           <div className={styles.dayName}>{dayName}</div>
           <div className={styles.dayNumber}>{dayNumber}</div>
+        </div>
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.bodyLeft}>
+          {times.map((time) => (
+            <div key={time} className={styles.timeSlot}>
+              {time}
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.bodyRight}>
+          {/* main content */}
         </div>
       </div>
     </div>
